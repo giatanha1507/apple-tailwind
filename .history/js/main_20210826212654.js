2274f6cfd9b98e244c0a313b6870e72e95cd9471
HaGiaTan <giatan15799@gmail.com>
2021-08-27 01:12:41 +0700

    apple-tailwind

---
+++ .history/js/main_20210826212654.js
@@ -0,0 +1,5 @@
+const toggleNavbar = (collapseID) => {
+  console.log(`helo`);
+};
+document.getElementById(collapseID).classList.toggle("hidden");
+document.getElementById(collapseID).classList.toggle("block");
